5d0b4ffe9c7648a16f844a155be747e69157b5c1
Rafał Paczocha <stainlessvoice@tutanota.com>
2019-06-05 18:21:47 +0200

    V7

--- WSumChecker/WSumChecker/MainWindow.xaml.cs
+++ WSumChecker/WSumChecker/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace WSumChecker
 {
@@ -89,6 +90,11 @@ namespace WSumChecker
                 {
                     if (File.Exists(CC_FFilePath.Text) == true)
                     {
+                        CC_FFileMD5.Text = "";
+                        CC_FFileSHA1.Text = "";
+                        CC_FFileSHA256.Text = "";
+                        CC_FFileSHA384.Text = "";
+                        CC_FFileSHA512.Text = "";
                         long CCFilelength = new System.IO.FileInfo(CC_FFilePath.Text).Length;
                         long CCSizeKB = 1024;
                         long CCSizeMB = 1024 * 1024;
@@ -101,33 +107,33 @@ namespace WSumChecker
                         if (CCSizeTBS >= 1)
                         {
                             CC_FFileSize.Text = CCSizeTBS.ToString() + " TB (" + CCFilelength + " B)";
-                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                         }
                         else if (CCSizeGBS >= 1)
                         {
                             CC_FFileSize.Text = CCSizeGBS.ToString() + " GB (" + CCFilelength + " B)";
-                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                         }
                         else if (CCSizeMBS >= 1)
                         {
                             CC_FFileSize.Text = CCSizeMBS.ToString() + " MB (" + CCFilelength + " B)";
-                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                         }
                         else if (CCSizeKBS >= 1)
                         {
                             CC_FFileSize.Text = CCSizeKBS.ToString() + " KB (" + CCFilelength + " B)";
-                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                         }
                         else
                         {
                             CC_FFileSize.Text = CCFilelength.ToString() + " B";
-                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                         }
                     }
                     else
                     {
                         CC_FFilePath.Text = "";
                         CC_FFileSize.Text = "";
+                        CC_FFileMD5.Text = "";
+                        CC_FFileSHA1.Text = "";
+                        CC_FFileSHA256.Text = "";
+                        CC_FFileSHA384.Text = "";
+                        CC_FFileSHA512.Text = "";
                         MessageBox.Show("There is no such file.", "File Input Error!", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
                 }
@@ -144,6 +150,11 @@ namespace WSumChecker
             {
                 if (File.Exists(CC_FFilePath.Text) == true)
                 {
+                    CC_FFileMD5.Text = "";
+                    CC_FFileSHA1.Text = "";
+                    CC_FFileSHA256.Text = "";
+                    CC_FFileSHA384.Text = "";
+                    CC_FFileSHA512.Text = "";
                     long CCFilelength = new System.IO.FileInfo(CC_FFilePath.Text).Length;
                     long CCSizeKB = 1024;
                     long CCSizeMB = 1024 * 1024;
@@ -156,35 +167,140 @@ namespace WSumChecker
                     if (CCSizeTBS >= 1)
                     {
                         CC_FFileSize.Text = CCSizeTBS.ToString() + " TB (" + CCFilelength + " B)";
-                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
                     else if (CCSizeGBS >= 1)
                     {
                         CC_FFileSize.Text = CCSizeGBS.ToString() + " GB (" + CCFilelength + " B)";
-                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
                     else if (CCSizeMBS >= 1)
                     {
                         CC_FFileSize.Text = CCSizeMBS.ToString() + " MB (" + CCFilelength + " B)";
-                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
                     else if (CCSizeKBS >= 1)
                     {
                         CC_FFileSize.Text = CCSizeKBS.ToString() + " KB (" + CCFilelength + " B)";
-                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
                     else
                     {
                         CC_FFileSize.Text = CCFilelength.ToString() + " B";
-                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
                 }
                 else
                 {
                     CC_FFilePath.Text = "";
                     CC_FFileSize.Text = "";
+                    CC_FFileMD5.Text = "";
+                    CC_FFileSHA1.Text = "";
+                    CC_FFileSHA256.Text = "";
+                    CC_FFileSHA384.Text = "";
+                    CC_FFileSHA512.Text = "";
                     MessageBox.Show("There is no such file.", "File Input Error!", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
+            }
+        }
+
+        private void CC_BFileHash_Click(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(CC_FFilePath.Text) == true)
+            {
+                if(CC_CFileMD5.IsChecked == true)
+                {
+                    string CCFILEI = @CC_FFilePath.Text;
+                    string CCFILEO;
+                    using (var md5 = MD5.Create())
+                    {
+                        using (var STREAM = File.OpenRead(CCFILEI))
+                        {
+                            byte[] CCCHECKSUMMD5 = md5.ComputeHash(STREAM);
+                            CCFILEO = BitConverter.ToString(CCCHECKSUMMD5).Replace("-", String.Empty).ToUpper();
+                            CC_FFileMD5.Text = CCFILEO;
+                        }
+                    }
+                }
+                else
+                {
+                    CC_FFileMD5.Text = "";
+                }
+
+                if (CC_CFileSHA1.IsChecked == true)
+                {
+                    string CCFILEI = @CC_FFilePath.Text;
+                    string CCFILEO;
+                    using (var sha1 = SHA1.Create())
+                    {
+                        using (var STREAM = File.OpenRead(CCFILEI))
+                        {
+                            byte[] CCCHECKSUMSHA1 = sha1.ComputeHash(STREAM);
+                            CCFILEO = BitConverter.ToString(CCCHECKSUMSHA1).Replace("-", String.Empty).ToUpper();
+                            CC_FFileSHA1.Text = CCFILEO;
+                        }
+                    }
+                }
+                else
+                {
+                    CC_FFileSHA1.Text = "";
+                }
+
+                if (CC_CFileSHA256.IsChecked == true)
+                {
+                    string CCFILEI = @CC_FFilePath.Text;
+                    string CCFILEO;
+                    using (var sha256 = SHA256.Create())
+                    {
+                        using (var STREAM = File.OpenRead(CCFILEI))
+                        {
+                            byte[] CCCHECKSUMSHA256 = sha256.ComputeHash(STREAM);
+                            CCFILEO = BitConverter.ToString(CCCHECKSUMSHA256).Replace("-", String.Empty).ToUpper();
+                            CC_FFileSHA256.Text = CCFILEO;
+                        }
+                    }
+                }
+                else
+                {
+                    CC_FFileSHA256.Text = "";
+                }
+
+                if (CC_CFileSHA384.IsChecked == true)
+                {
+                    string CCFILEI = @CC_FFilePath.Text;
+                    string CCFILEO;
+                    using (var sha384 = SHA384.Create())
+                    {
+                        using (var STREAM = File.OpenRead(CCFILEI))
+                        {
+                            byte[] CCCHECKSUMSHA384 = sha384.ComputeHash(STREAM);
+                            CCFILEO = BitConverter.ToString(CCCHECKSUMSHA384).Replace("-", String.Empty).ToUpper();
+                            CC_FFileSHA384.Text = CCFILEO;
+                        }
+                    }
+                }
+                else
+                {
+                    CC_FFileSHA384.Text = "";
+                }
+
+                if (CC_CFileSHA512.IsChecked == true)
+                {
+                    string CCFILEI = @CC_FFilePath.Text;
+                    string CCFILEO;
+                    using (var sha512 = SHA512.Create())
+                    {
+                        using (var STREAM = File.OpenRead(CCFILEI))
+                        {
+                            byte[] CCCHECKSUMSHA512 = sha512.ComputeHash(STREAM);
+                            CCFILEO = BitConverter.ToString(CCCHECKSUMSHA512).Replace("-", String.Empty).ToUpper();
+                            CC_FFileSHA512.Text = CCFILEO;
+                        }
+                    }
+                }
+                else
+                {
+                    CC_FFileSHA512.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("You did not choose any files.", "File Input Error!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
