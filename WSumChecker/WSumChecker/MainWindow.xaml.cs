366035821345a90a52fd4a9b67c37ad5fadca0de
Rafał Paczocha <stainlessvoice@tutanota.com>
2019-06-05 16:47:57 +0200

    v5

--- WSumChecker/WSumChecker/MainWindow.xaml.cs
+++ WSumChecker/WSumChecker/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.IO;
 
 namespace WSumChecker
 {
@@ -29,8 +30,162 @@ namespace WSumChecker
         private void CC_BFileSearch_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ccplik = new OpenFileDialog();
-            ccplik.ShowDialog();
-            CC_FFilePath.Text = ccplik.FileName;
+            ccplik.Filter = "All files (*.*)|*.*";
+            ccplik.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            try
+            {
+                ccplik.ShowDialog();
+                CC_FFilePath.Text = ccplik.FileName;
+                long CCFilelength = new System.IO.FileInfo(ccplik.FileName).Length;
+                long CCSizeKB = 1024;
+                long CCSizeMB = 1024 * 1024;
+                long CCSizeGB = 1024 * 1024 * 1024;
+                long CCSizeTB = CCSizeMB * CCSizeMB;
+                double CCSizeKBS = CCFilelength / CCSizeKB;
+                double CCSizeMBS = CCFilelength / CCSizeMB;
+                double CCSizeGBS = CCFilelength / CCSizeGB;
+                double CCSizeTBS = CCFilelength / CCSizeTB;
+                if (CCSizeTBS >= 1)
+                {
+                    CC_FFileSize.Text = CCSizeTBS.ToString() + " TB (" + CCFilelength + " B)";
+                    MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if (CCSizeGBS >= 1)
+                {
+                    CC_FFileSize.Text = CCSizeGBS.ToString() + " GB (" + CCFilelength + " B)";
+                    MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if (CCSizeMBS >= 1)
+                {
+                    CC_FFileSize.Text = CCSizeMBS.ToString() + " MB (" + CCFilelength + " B)";
+                    MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else if (CCSizeKBS >= 1)
+                {
+                    CC_FFileSize.Text = CCSizeKBS.ToString() + " KB (" + CCFilelength + " B)";
+                    MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    CC_FFileSize.Text = CCFilelength.ToString() + " B";
+                    MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (System.ArgumentException exception)
+            {
+                CC_FFileSize.Text = "";
+                MessageBox.Show("No file has been chosen.", "Opening File Cancelled!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void CC_FFilePath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (CC_FFilePath.Text == "")
+                {
+                    CC_FFilePath.Text = "";
+                }
+                else
+                {
+                    if (File.Exists(CC_FFilePath.Text) == true)
+                    {
+                        long CCFilelength = new System.IO.FileInfo(CC_FFilePath.Text).Length;
+                        long CCSizeKB = 1024;
+                        long CCSizeMB = 1024 * 1024;
+                        long CCSizeGB = 1024 * 1024 * 1024;
+                        long CCSizeTB = CCSizeMB * CCSizeMB;
+                        double CCSizeKBS = CCFilelength / CCSizeKB;
+                        double CCSizeMBS = CCFilelength / CCSizeMB;
+                        double CCSizeGBS = CCFilelength / CCSizeGB;
+                        double CCSizeTBS = CCFilelength / CCSizeTB;
+                        if (CCSizeTBS >= 1)
+                        {
+                            CC_FFileSize.Text = CCSizeTBS.ToString() + " TB (" + CCFilelength + " B)";
+                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else if (CCSizeGBS >= 1)
+                        {
+                            CC_FFileSize.Text = CCSizeGBS.ToString() + " GB (" + CCFilelength + " B)";
+                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else if (CCSizeMBS >= 1)
+                        {
+                            CC_FFileSize.Text = CCSizeMBS.ToString() + " MB (" + CCFilelength + " B)";
+                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else if (CCSizeKBS >= 1)
+                        {
+                            CC_FFileSize.Text = CCSizeKBS.ToString() + " KB (" + CCFilelength + " B)";
+                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else
+                        {
+                            CC_FFileSize.Text = CCFilelength.ToString() + " B";
+                            MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+                    else
+                    {
+                        CC_FFilePath.Text = "";
+                        CC_FFileSize.Text = "";
+                        MessageBox.Show("There is no such file.", "File Input Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+
+        private void CC_FFilePath_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (CC_FFilePath.Text == "")
+            {
+                CC_FFilePath.Text = "";
+            }
+            else
+            {
+                if (File.Exists(CC_FFilePath.Text) == true)
+                {
+                    long CCFilelength = new System.IO.FileInfo(CC_FFilePath.Text).Length;
+                    long CCSizeKB = 1024;
+                    long CCSizeMB = 1024 * 1024;
+                    long CCSizeGB = 1024 * 1024 * 1024;
+                    long CCSizeTB = CCSizeMB * CCSizeMB;
+                    double CCSizeKBS = CCFilelength / CCSizeKB;
+                    double CCSizeMBS = CCFilelength / CCSizeMB;
+                    double CCSizeGBS = CCFilelength / CCSizeGB;
+                    double CCSizeTBS = CCFilelength / CCSizeTB;
+                    if (CCSizeTBS >= 1)
+                    {
+                        CC_FFileSize.Text = CCSizeTBS.ToString() + " TB (" + CCFilelength + " B)";
+                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else if (CCSizeGBS >= 1)
+                    {
+                        CC_FFileSize.Text = CCSizeGBS.ToString() + " GB (" + CCFilelength + " B)";
+                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else if (CCSizeMBS >= 1)
+                    {
+                        CC_FFileSize.Text = CCSizeMBS.ToString() + " MB (" + CCFilelength + " B)";
+                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else if (CCSizeKBS >= 1)
+                    {
+                        CC_FFileSize.Text = CCSizeKBS.ToString() + " KB (" + CCFilelength + " B)";
+                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        CC_FFileSize.Text = CCFilelength.ToString() + " B";
+                        MessageBox.Show("File loaded and ready.", "File Loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+                else
+                {
+                    CC_FFilePath.Text = "";
+                    CC_FFileSize.Text = "";
+                    MessageBox.Show("There is no such file.", "File Input Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
